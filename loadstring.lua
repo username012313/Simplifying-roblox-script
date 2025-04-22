bbec1104ad92ae276e4eb44e2c0d8a652175c54a
username012313 <lamgay1231213131223321@gmail.com>
2025-04-23 00:08:15 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source', true))() 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow62x/catbypass/main/upfix"))() 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wisl884/wisl-i-Universal-Project1/refs/heads/main/Wisl'i%20Universal%20Project%20Mobile.lua"))()
+loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))() 
