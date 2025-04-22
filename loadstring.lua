7fe97ac7f5623ee4d594ccb6e4d7098a6737a3e8
username012313 <lamgay1231213131223321@gmail.com>
2025-04-22 23:50:11 +0200

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source', true))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow62x/catbypass/main/upfix"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wisl884/wisl-i-Universal-Project1/refs/heads/main/Wisl'i%20Universal%20Project%20new%20UI.lua", true))()
