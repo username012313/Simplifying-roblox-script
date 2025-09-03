8ca04853a188cc231c12ba40d07e60bf3c653194
username012313 <lamgay1231213131223321@gmail.com>
2025-09-04 01:56:18 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source', true))() 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MariyaFurmanova/Library/main/dex2.0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/peyton2465/Dex/master/out.lua"))()
